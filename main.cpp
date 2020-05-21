6b3a2b8ecbea65f03b6877b44a5276ccc90761ec
Calandrinon <georgesut@yahoo.com>
2020-05-21 17:26:06 +0300

    Testing config file reading

--- main.cpp
+++ main.cpp
@@ -1,19 +1,56 @@
 #include <iostream>
+#include <vector>
 #include "tests.h"
 #include "ui.h"
 //#include <crtdbg.h>
 #include "gui.h"
+#include "dialog.h"
 #include <memory>
 #include <QApplication>
+#include <fstream>
+#include "string_functions.h"
+#include <QDebug>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
     //Tests();
     QApplication a(argc, argv);
-    FileRepository repository("in.in");
-    //MemoryRepository repository;
-    Service service(&repository);
+
+    Dialog dialog;
+    dialog.exec();
+
+    ifstream in("config.txt");
+    string line, repo_type="MemoryRepository";
+
+    while (getline(in, line)) {
+        qDebug() << "wtf";
+        if (line[0] == '#') {
+            qDebug() << QString::fromStdString(line);
+            continue;
+        } else {
+            line = StringFunctions::strip(line);
+            vector<string> tokens = StringFunctions::tokenize(line, '=');
+
+            for (auto token: tokens) {
+                token = StringFunctions::strip(token);
+            }
+
+            if (tokens[0] == "repo_type") {
+                repo_type = tokens[1];
+            }
+        }
+    }
+
+
+    Repository* repository;
+    if (repo_type == "FileRepository") {
+        repository = new FileRepository("in.in");
+    } else {
+        repository = new MemoryRepository();
+    }
+
+    Service service(repository);
     GUI gui(&service);
     gui.show();
 
